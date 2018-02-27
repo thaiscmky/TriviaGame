49e73e3344494d4fcc3ffcff459af9e89a0429fd
Thais Cailet <thais.cailet@gmail.com>
2018-02-26 21:39:31 -0600

    Completes #10 Adds barebone structure for the game's JavaScript object.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,50 @@
-/**
- * Created by dynom on 2/26/2018.
- */
+
+var game = {
+    score: {
+        correct:0,
+        incorrect:0,
+        unanswered:0
+    },
+    groups: [
+        {
+            question: '',
+            answer: '',
+            choices: {
+                correct: [],
+                incorrect: []
+            }
+        }
+    ],
+    timer: {
+        countFrom: 30,
+        countTo: 0,
+        currentInterval: 0,
+        currentTime: 30,
+        convertime: function(t, type){
+            var convertedTime = t;
+            switch(type){
+                case 'ms2sec':
+                    break;
+                case 'sec2ms':
+                    break;
+                break;
+            }
+            return convertedTime;
+        },
+        restartInterval: function(){
+
+        },
+        pauseInterval: function(){
+
+        }
+    },
+    showAnswer: function(){
+
+    },
+    nextQuestion: function(){
+
+    },
+    restartGame: function(){
+
+    }
+};
